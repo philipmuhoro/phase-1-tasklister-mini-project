f2379e5cbd978132418b9e9fd4b2d78245e707c4
philipwanjiku <muhorophilip7@gmail.com>
2023-10-30 10:23:14 +0300

    Completed Project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
 });
+  let form = document.querySelector('form');
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    handleTodo(e.target.new_task.value);
+    form.reset();
+  });
+
+
+
+function handleTodo(todo){
+
+  let p = document.createElement('p');
+  let btn = document.createElement('button')
+  document.addEventListener('click', deleteTodo)
+
+  btn.textContent = 'Delete'
+
+  p.textContent = `${todo} `;
+  p.appendChild(btn)
+  console.log(p)
+  document.querySelector('#list').appendChild(p);
+
+}
+
+function deleteTodo(e){
+  e.target.parentNode.remove();
+}
